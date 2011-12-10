e3819cc51faa05fbfbc14af4e689f4c5a71dabec
Ryan Swanstrom <git@swgoof.com>
2011-12-10 14:41:34 -0600

    adding user accounts

--- app/controllers/Comments.java
+++ app/controllers/Comments.java
@@ -8,15 +8,16 @@ import play.mvc.Controller;
 public class Comments extends Controller {
     
     public static void create(String ideaId, Comment comment) {
-        Idea idea = Idea.findById(ideaId);
-        notFoundIfNull(idea);
-        comment.idea = idea;
-        if (!comment.validateAndCreate()) {            
-            params.flash();
-            Validation.keep();
-        }
-        
-        Ideas.show(idea.topic.path, idea.path);
+        Application.index();
+//        Idea idea = Idea.findById(ideaId);
+//        notFoundIfNull(idea);
+//        comment.idea = idea;
+//        if (!comment.validateAndCreate()) {            
+//            params.flash();
+//            Validation.keep();
+//        }
+//        
+//        Ideas.show(idea.topic.path, idea.path);
         
     }
     
---
+++ app/models/User.java
@@ -0,0 +1,45 @@
+package models;
+
+import com.google.code.morphia.annotations.Entity;
+import com.google.code.morphia.annotations.Indexed;
+import com.google.code.morphia.utils.IndexDirection;
+import java.util.Date;
+import java.util.UUID;
+import play.data.validation.Email;
+import play.data.validation.Required;
+
+@Entity
+public class User extends BaseModel {
+
+    @Required
+    @Indexed(value=IndexDirection.ASC, name="idx_username", unique=true, dropDups=true) 
+    public String username;
+    @Email
+    @Required
+    @Indexed(value=IndexDirection.ASC, name="idx_email", unique=true, dropDups=true) 
+    public String email;
+    @Required
+    public String password;
+    @Required
+    public String salt;
+    public Integer hashIters;
+    public Date lastLogin;
+    public Valid active;
+    public String uuid;
+
+    public User() {
+        this.active = Valid.N;
+        this.uuid = UUID.randomUUID().toString();
+        this.created = new Date();
+    }
+
+    public void activate() {
+        this.active = Valid.Y;
+        this.save();
+    }
+
+    @Override
+    public String toString() {
+        return this.username;
+    }
+}
