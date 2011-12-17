c574322ab0edf8c001f73fe9ffe736ea4a97799e
Ryan Swanstrom <git@swgoof.com>
2011-12-17 16:07:03 -0600

    adding IdeasSecure

---
+++ app/controllers/IdeasSecure.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import models.Idea;
+import models.Topic;
+import models.User;
+import models.Valid;
+import play.data.validation.Validation;
+import play.mvc.Controller;
+import play.mvc.With;
+
+/**
+ *
+ * @author goof
+ */
+@With(Secure.class)
+public class IdeasSecure extends Controller {
+    
+    /**
+     *
+     * @param path the topic path
+     */
+    public static void add(String path) {
+        Topic topic = Topic.find("byPathAndValid", path, Valid.Y).first();
+        notFoundIfNull(topic);
+        Idea idea = new Idea();
+        idea.topic = topic;
+        render(idea);
+    }
+
+    /**
+     *
+     * @param topicId
+     * @param txt
+     */
+    public static void create(String topicId, String txt) {
+        Topic topic = Topic.findById(topicId);
+        notFoundIfNull(topic);
+        Idea idea = new Idea();
+        idea.txt = txt;
+        idea.topic = topic;
+        idea.user = User.find("byUsernameAndValid", Security.connected(), Valid.Y).first();
+
+        if (!idea.validateAndSave() ) {
+            params.flash();
+            Validation.keep();
+            add(topic.path);
+        }
+        Ideas.show(idea.topic.path, idea.path);
+    }
+    
+}
