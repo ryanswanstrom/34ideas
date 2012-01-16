d9381fb6ea9003f4aba5a91b680437755817202e
Ryan Swanstrom <git@swgoof.com>
2012-01-16 10:37:56 -0600

    minor updates

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -3,6 +3,7 @@ import models.Idea;
 import play.jobs.Job;
 import play.jobs.OnApplicationStart;
 import play.modules.morphia.Model;
+import play.test.MorphiaFixtures;
 
 /*
  * To change this template, choose Tools | Templates
@@ -18,12 +19,12 @@ public class Bootstrap extends Job {
     @Override
     public void doJob() {
         if (play.Play.mode == play.Play.mode.DEV) {
-//            System.out.println("delete all");
-//            MorphiaFixtures.deleteAllModels();
+            System.out.println("delete all");
+            MorphiaFixtures.deleteAllModels();
         }
         if (Idea.count() == 0) {
-//            System.out.println("loading yaml");
-//            MorphiaFixtures.loadModels("initial-data.yml");
+            System.out.println("loading yaml");
+            MorphiaFixtures.loadModels("initial-data.yml");
         }
         Model.ds().ensureIndexes();
     }
